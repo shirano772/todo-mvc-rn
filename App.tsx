1e1dcf043349525b24467b9e9d98e9292a645a05
s_hirano <s_hirano@waku-2.com>
2021-02-16 12:14:14 +0900

    タブアイコンを実装

--- App.tsx
+++ App.tsx
@@ -3,15 +3,30 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AddTodo } from './src/app/components/AddTodo';
 import { TodoList } from './src/app/components/TodoList';
+import { AntDesign } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
+
+const AddTodoIcon: React.FC<{size: number, color: string}> = ({color, size}) =>
+(<AntDesign name="pluscircle" size={size} color={color} />)
+
+const TodoListIcon: React.FC<{size: number, color: string}> = ({color, size}) =>
+(<AntDesign name="infocirlce" size={size} color={color} />)
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="タスク作成" component={AddTodo} />
-        <Tab.Screen name="タスク一覧" component={TodoList} />
+        <Tab.Screen
+        name="タスク作成"
+        component={AddTodo}
+        options={{tabBarIcon: AddTodoIcon}}
+        />
+        <Tab.Screen
+        name="タスク一覧"
+        component={TodoList}
+        options={{tabBarIcon: TodoListIcon}}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
